1d2d22b6488dac7fc36313859c0c4411a7ea0971
aserban <aserban@suvoda.com>
2020-03-08 18:11:56 +0200

    First commit

---
+++ LicentaPrototip/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LicentaPrototip.MvcApplication" Language="C#" %>
---
+++ LicentaPrototip/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LicentaPrototip.Startup))]
+namespace LicentaPrototip
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
